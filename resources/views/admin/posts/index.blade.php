c7b08f5e5dec84a6ebcb243bc3926abe73d1fbbd
Mohamed Ismail <mohamedismail0994@gmail.com>
2018-09-26 15:26:22 +0200

    Now Create Post with Dynamic Category is Available

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -29,7 +29,7 @@
                 <td>{{$post->id}}</td>
                 <td><img height="70" src="{{$post->photo ? $post->photo->file : '/images/no_image_placeholder.jpg'}}" alt=""></td>
                 <td>{{$post->user->name}}</td>
-                <td>{{$post->category_id}}</td>
+                <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
                 <td>{{$post->title}}</td>
                 <td>{{$post->body}}</td>
                 <td>{{$post->created_at->diffForHumans()}}</td>
