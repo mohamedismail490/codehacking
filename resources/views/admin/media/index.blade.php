bf70f6bb89081019b7b85b4a2fa2fba84c34158f
Mohamed Ismail <mohamedismail0994@gmail.com>
2018-09-28 10:24:48 +0200

    Now Media Section is ready

---
+++ resources/views/admin/media/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    @if(Session::has('deleted_photo'))
+
+        <p class="alert alert-danger">{{session('deleted_photo')}}</p>
+
+    @endif
+
+    <h1>Media</h1>
+
+    @if($photos)
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>PHOTO</th>
+                <th>CREATED</th>
+                <th>UPDATED</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            @foreach($photos as $photo)
+
+                <tr>
+                    <td>{{$photo -> id}}</td>
+                    <td><img height="75" src="{{$photo -> file}}" class="img-rounded" alt=""></td>
+                    <td>{{$photo -> created_at -> diffForHumans()}}</td>
+                    <td>{{$photo -> updated_at -> diffForHumans()}}</td>
+                    <td>
+
+                        {!! Form::open(['method'=>'DELETE', 'action'=>['AdminMediasController@destroy', $photo->id]]) !!}
+
+                            <div class="form-group">
+                                {!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+                            </div>
+
+                        {!! Form::close() !!}
+
+
+                    </td>
+                </tr>
+
+            @endforeach
+            </tbody>
+        </table>
+
+    @endif
+
+@stop
